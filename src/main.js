22a8e2e3d2bfaef5bd79a3de5ec6b6e09a4affcf
luhaiYang <1508217927@qq.com>
2022-02-26 15:59:48 +0800

    delete jq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import jquery from 'jQuery'
 import App from './App.vue'
 import './index.css'
 
@@ -9,6 +8,6 @@ import "bootstrap/dist/css/bootstrap.css"
 
 import router from './router'
 
-const app = createApp(App).use(jquery)
+const app = createApp(App)
 app.use(router)
 app.mount('#app')
